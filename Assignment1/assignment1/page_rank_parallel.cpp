64daba1d40489ba0a65046afde778c1b207e87f1
Raghav Mittal <rmittal@sfu.ca>
2019-09-15 23:39:11 -0700

    A3 part 3 threading done

--- Assignment1/assignment1/page_rank_parallel.cpp
+++ Assignment1/assignment1/page_rank_parallel.cpp
@@ -4,6 +4,9 @@
 #include "utils.h"
 #include "graph.h"
 
+#include <thread>
+#include <vector>
+
 #ifdef USE_INT
 #define INIT_PAGE_RANK 100000
 #define EPSILON 1000
@@ -19,9 +22,14 @@ typedef int64_t PageRankType;
 typedef float PageRankType;
 #endif
 
+void threadFunction() {
+    std::cout<<"I am inside the thread function"<<std::endl;
+}
+
 void pageRankSerial(Graph &g, int max_iters)
 {
     uintV n = g.n_;
+    std::cout<<"Number of nodes: " << n << std::endl;
 
     PageRankType *pr_curr = new PageRankType[n];
     PageRankType *pr_next = new PageRankType[n];
@@ -37,28 +45,65 @@ void pageRankSerial(Graph &g, int max_iters)
     double time_taken = 0.0;
     // Create threads and distribute the work across T threads
     // -------------------------------------------------------------------
+    //Lets take the number of threads to be 4 to start.
+    std::thread threadList[4];
     t1.start();
-    for (int iter = 0; iter < max_iters; iter++)
-    {
-        // for each vertex 'u', process all its outNeighbors 'v'
-        for (uintV u = 0; u < n; u++)
-        {
-            uintE out_degree = g.vertices_[u].getOutDegree();
-            for (uintE i = 0; i < out_degree; i++)
-            {
-                uintV v = g.vertices_[u].getOutNeighbor(i);
-                pr_next[v] += (pr_curr[u] / out_degree);
-            }
-        }
-        for (uintV v = 0; v < n; v++)
-        {
-            pr_next[v] = PAGE_RANK(pr_next[v]);
 
-            // reset pr_curr for the next iteration
-            pr_curr[v] = pr_next[v];
-            pr_next[v] = 0.0;
-        }
+    int start = 0, eachWorkload = n/4;
+    for(int i = 0; i < 4; i++) {
+        threadList[i] = std::thread ([&](){
+            for (int iter = start; iter < start + eachWorkload; iter++) {
+                // for each vertex 'u', process all its outNeighbors 'v'
+                for (uintV u = 0; u < n; u++)
+                {
+                    uintE out_degree = g.vertices_[u].getOutDegree();
+                    for (uintE i = 0; i < out_degree; i++)
+                    {
+                        uintV v = g.vertices_[u].getOutNeighbor(i);
+                        //Put a lock here.
+                        pr_next[v] += (pr_curr[u] / out_degree);
+                        //Unlock it here.
+                    }
+                }
+                for (uintV v = 0; v < n; v++)
+                {
+                    pr_next[v] = PAGE_RANK(pr_next[v]);
+
+                    // reset pr_curr for the next iteration
+                    pr_curr[v] = pr_next[v];
+                    pr_next[v] = 0.0;
+                }
+            }
+        });
+        start += eachWorkload;
     }
+
+    for(auto &t : threadList) {
+        t.join();
+        std::cout<<"Joined a thread"<<std::endl;
+    }
+
+    // for (int iter = 0; iter < max_iters; iter++)
+    // {
+    //     // for each vertex 'u', process all its outNeighbors 'v'
+    //     for (uintV u = 0; u < n; u++)
+    //     {
+    //         uintE out_degree = g.vertices_[u].getOutDegree();
+    //         for (uintE i = 0; i < out_degree; i++)
+    //         {
+    //             uintV v = g.vertices_[u].getOutNeighbor(i);
+    //             pr_next[v] += (pr_curr[u] / out_degree);
+    //         }
+    //     }
+    //     for (uintV v = 0; v < n; v++)
+    //     {
+    //         pr_next[v] = PAGE_RANK(pr_next[v]);
+
+    //         // reset pr_curr for the next iteration
+    //         pr_curr[v] = pr_next[v];
+    //         pr_next[v] = 0.0;
+    //     }
+    // }
     time_taken = t1.stop();
     // -------------------------------------------------------------------
     // std::cout << "thread_id, time_taken\n";
@@ -73,9 +118,10 @@ void pageRankSerial(Graph &g, int max_iters)
         sum_of_page_ranks += pr_curr[u];
     }
     std::cout << "Sum of page rank : " << sum_of_page_ranks << "\n";
+
     std::cout << "Time taken (in seconds) : " << time_taken << "\n";
-    delete[] pr_curr;
-    delete[] pr_next;
+    // delete[] pr_curr;
+    // delete[] pr_next;
 }
 
 int main(int argc, char *argv[])
@@ -102,8 +148,9 @@ int main(int argc, char *argv[])
 
     Graph g;
     std::cout << "Reading graph\n";
-    std::cout<<"Page rank serial"<<std::endl;
+    std::cout << input_file_path << std::endl;
     g.read_graph_from_binary<int>(input_file_path);
+    std::cout<<"Read the graph"<<std::endl;
     std::cout << "Created graph\n";
     pageRankSerial(g, max_iterations);
 
