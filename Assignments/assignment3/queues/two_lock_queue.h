c233da25d74774aa80e530de94e16ac0646e511c
Raghav Mittal <rmittal@sfu.ca>
2019-10-30 17:49:29 -0700

    Wrote code part 2

--- Assignments/assignment3/queues/two_lock_queue.h
+++ Assignments/assignment3/queues/two_lock_queue.h
@@ -1,14 +1,23 @@
 #include "../common/allocator.h"
+#include <mutex>
 
 template <class T>
 class Node
 {
+    public:
+        T value;
+        Node<T>* next;    
 };
 
 template <class T>
 class TwoLockQueue
 {
+    Node<T>* q_head;
+    Node<T>* q_tail;
     CustomAllocator my_allocator_;
+
+    std::mutex enq_lock;
+    std::mutex deq_lock;
 
 public:
     TwoLockQueue() : my_allocator_()
@@ -21,17 +30,38 @@ public:
         my_allocator_.initialize(t_my_allocator_size, sizeof(Node<T>));
         // Initialize the queue head or tail here
         Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
-        my_allocator_.freeNode(newNode);
+        newNode->next = nullptr;
+        q_head = newNode;
+        q_tail = newNode;
+        // my_allocator_.freeNode(newNode);
     }
 
     void enqueue(T value)
     {
+        Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
+        newNode->value = value;
+        newNode->next = nullptr;
+
+        enq_lock.lock();
+        q_tail->next = newNode;
+        q_tail = newNode;
+        enq_lock.unlock();
     }
 
     bool dequeue(T *value)
     {
-        bool ret_value = false;
-        return ret_value;
+        deq_lock.lock();
+        Node<T>* node = q_head;
+        Node<T>* new_head = q_head->next;
+        if(new_head == nullptr) {
+            deq_lock.unlock();
+            return false;
+        }
+        *value = new_head->value;
+        q_head = new_head;
+        my_allocator_.freeNode(node);
+        deq_lock.unlock();
+        return true;
     }
 
     void cleanup()
