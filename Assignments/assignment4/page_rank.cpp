c33b529cb1047c477771ff8e1cf466d0d7123ae2
Raghav Mittal <rmittal@sfu.ca>
2019-11-20 22:31:58 -0800

    Add a4

---
+++ Assignments/assignment4/page_rank.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include "core/utils.h"
+#include "core/graph.h"
+
+#ifdef USE_INT
+#define INIT_PAGE_RANK 100000
+#define EPSILON 1000
+#define PAGE_RANK(x) (15000 + (5 * x) / 6)
+#define CHANGE_IN_PAGE_RANK(x, y) std::abs(x - y)
+#define PAGERANK_MPI_TYPE MPI_LONG
+typedef int64_t PageRankType;
+#else
+#define INIT_PAGE_RANK 1.0
+#define EPSILON 0.01
+#define DAMPING 0.85
+#define PAGE_RANK(x) (1 - DAMPING + DAMPING * x)
+#define CHANGE_IN_PAGE_RANK(x, y) std::fabs(x - y)
+#define PAGERANK_MPI_TYPE MPI_FLOAT
+typedef float PageRankType;
+#endif
+
+void pageRankSerial(Graph &g, int max_iters)
+{
+    uintV n = g.n_;
+    double time_taken;
+    timer t1;
+    PageRankType *pr_curr = new PageRankType[n];
+    PageRankType *pr_next = new PageRankType[n];
+
+    t1.start();
+    for (uintV i = 0; i < n; i++)
+    {
+        pr_curr[i] = INIT_PAGE_RANK;
+        pr_next[i] = 0.0;
+    }
+
+    // Push based pagerank
+    // -------------------------------------------------------------------
+    for (int iter = 0; iter < max_iters; iter++)
+    {
+        // for each vertex 'u', process all its outNeighbors 'v'
+        for (uintV u = 0; u < n; u++)
+        {
+            uintE out_degree = g.vertices_[u].getOutDegree();
+            for (uintE i = 0; i < out_degree; i++)
+            {
+                uintV v = g.vertices_[u].getOutNeighbor(i);
+                pr_next[v] += (pr_curr[u] / out_degree);
+            }
+        }
+        for (uintV v = 0; v < n; v++)
+        {
+            pr_next[v] = PAGE_RANK(pr_next[v]);
+
+            // reset pr_curr for the next iteration
+            pr_curr[v] = pr_next[v];
+            pr_next[v] = 0.0;
+        }
+    }
+    // -------------------------------------------------------------------
+
+    // For every thread, print the following statistics:
+    // rank, num_edges, communication_time
+    // 0, 344968860, 1.297778
+    // 1, 344968860, 1.247763
+    // 2, 344968860, 0.956243
+    // 3, 344968880, 0.467028
+
+    PageRankType sum_of_page_ranks = 0;
+    for (uintV u = 0; u < n; u++)
+    {
+        sum_of_page_ranks += pr_curr[u];
+    }
+    time_taken = t1.stop();
+    std::cout << "Sum of page rank : " << sum_of_page_ranks << "\n";
+    std::cout << "Time taken (in seconds) : " << std::setprecision(TIME_PRECISION) << time_taken << "\n";
+    delete[] pr_curr;
+    delete[] pr_next;
+}
+
+int main(int argc, char *argv[])
+{
+    cxxopts::Options options("page_rank_push", "Calculate page_rank using serial and parallel execution");
+    options.add_options("", {
+                                {"nIterations", "Maximum number of iterations", cxxopts::value<uint>()->default_value(DEFAULT_MAX_ITER)},
+                                {"strategy", "Strategy to be used", cxxopts::value<uint>()->default_value(DEFAULT_STRATEGY)},
+                                {"inputFile", "Input graph file path", cxxopts::value<std::string>()->default_value("/scratch/assignment1/input_graphs/roadNet-CA")},
+                            });
+
+    auto cl_options = options.parse(argc, argv);
+    uint strategy = cl_options["strategy"].as<uint>();
+    uint max_iterations = cl_options["nIterations"].as<uint>();
+    std::string input_file_path = cl_options["inputFile"].as<std::string>();
+
+#ifdef USE_INT
+    std::cout << "Using INT\n";
+#else
+    std::cout << "Using FLOAT\n";
+#endif
+    std::cout << std::fixed;
+    // Get the world size and print it out here
+    // std::cout << "World size : " << world_size << "\n"
+    std::cout << "Communication strategy : " << strategy << "\n";
+    std::cout << "Iterations : " << max_iterations << "\n";
+
+    Graph g;
+    g.readGraphFromBinary<int>(input_file_path);
+
+    switch (strategy)
+    {
+    case 0:
+        pageRankSerial(g, max_iterations);
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
---
+++ Assignments/assignment4/triangle_counting.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include "core/utils.h"
+#include "core/graph.h"
+
+uintV countTriangles(uintV *array1, uintE len1, uintV *array2, uintE len2, uintV u, uintV v)
+{
+
+    uintE i = 0, j = 0; // indexes for array1 and array2
+    uintV count = 0;
+
+    if (u == v)
+        return count;
+
+    while ((i < len1) && (j < len2))
+    {
+        if (array1[i] == array2[j])
+        {
+            if ((array1[i] != u) && (array1[i] != v))
+            {
+                count++;
+            }
+            else
+            {
+                // triangle with self-referential edge -> ignore
+            }
+            i++;
+            j++;
+        }
+        else if (array1[i] < array2[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+    return count;
+}
+
+void triangleCountSerial(Graph &g)
+{
+    uintV n = g.n_;
+    long triangle_count = 0;
+    double time_taken;
+    timer t1;
+    t1.start();
+    for (uintV u = 0; u < n; u++)
+    {
+        uintE out_degree = g.vertices_[u].getOutDegree();
+        for (uintE i = 0; i < out_degree; i++)
+        {
+            uintV v = g.vertices_[u].getOutNeighbor(i);
+            triangle_count += countTriangles(g.vertices_[u].getInNeighbors(),
+                                             g.vertices_[u].getInDegree(),
+                                             g.vertices_[v].getOutNeighbors(),
+                                             g.vertices_[v].getOutDegree(),
+                                             u,
+                                             v);
+        }
+    }
+
+    // For every thread, print out the following statistics:
+    // rank, edges, triangle_count, communication_time
+    // 0, 17248443, 144441858, 0.000074
+    // 1, 17248443, 152103585, 0.000020
+    // 2, 17248443, 225182666, 0.000034
+    // 3, 17248444, 185596640, 0.000022
+
+    time_taken = t1.stop();
+
+    // Print out overall statistics
+    std::cout << "Number of triangles : " << triangle_count << "\n";
+    std::cout << "Number of unique triangles : " << triangle_count / 3 << "\n";
+    std::cout << "Time taken (in seconds) : " << std::setprecision(TIME_PRECISION) << time_taken << "\n";
+}
+
+int main(int argc, char *argv[])
+{
+    cxxopts::Options options("triangle_counting_serial", "Count the number of triangles using serial and parallel execution");
+    options.add_options("custom", {
+                                      {"strategy", "Strategy to be used", cxxopts::value<uint>()->default_value(DEFAULT_STRATEGY)},
+                                      {"inputFile", "Input graph file path", cxxopts::value<std::string>()->default_value("/scratch/assignment1/input_graphs/roadNet-CA")},
+                                  });
+
+    auto cl_options = options.parse(argc, argv);
+    uint strategy = cl_options["strategy"].as<uint>();
+    std::string input_file_path = cl_options["inputFile"].as<std::string>();
+
+    std::cout << std::fixed;
+    // Get the world size and print it out here
+    // std::cout << "World size : " << world_size << "\n";
+    std::cout << "Communication strategy : " << strategy << "\n";
+
+    Graph g;
+    g.readGraphFromBinary<int>(input_file_path);
+
+    timer t1;
+    t1.start();
+    switch (strategy)
+    {
+    case 0:
+        triangleCountSerial(g);
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
+
